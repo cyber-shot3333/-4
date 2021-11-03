7c12ba1f07677043a42976a3767ec38d92502a9e
I.Smirnov <I.Smirnov@51JGJF3>
2021-11-03 10:18:26 +0300

    Добавьте файлы проекта.

---
+++ Lab4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopFor
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Число N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            for (int i = 1; i <= n; i++)
+            Console.WriteLine(i + ": " + i * i);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ LoopWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopWhile
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int MyValue = 0;
+            List<int> myList = new List<int>();
+            do
+            {
+                MyValue = Convert.ToInt16(Console.ReadLine());
+
+                if (MyValue < 0) myList.Add(MyValue);
+            } while (MyValue != 0);
+
+            if (myList.Count < 2)
+                Console.WriteLine("Последовательность содержит меньше 2 отрицательных элементов");
+            else
+                Console.WriteLine("Предпоследний отрицательный элемент = {0}", myList[myList.Count - 2]);
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
